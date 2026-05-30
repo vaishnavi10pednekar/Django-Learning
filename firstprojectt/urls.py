5364f8e0017fee2fed22e2afbdf2ed7cbdb20934
Vaishnavi Pednekar <vaishnavi10pednekar@gmail.com>
2026-05-30 16:11:48 +0530

    updated project

--- firstprojectt/urls.py
+++ firstprojectt/urls.py
@@ -5,7 +5,10 @@ from firstprojectt import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('task/', views.task, name="task"),
+    path('task/del_task/<task_id>', views.del_task, name="del_task"),
+    path('task/edit_task/<task_id>', views.edit_task, name="edit_task"),
+    path('task/comp_task/<task_id>', views.comp_task, name="comp_task"),
+     path('task/pend_task/<task_id>', views.pend_task, name="pend_task"),
     path('aboutus/', views.aboutus, name="aboutus"),
     path('contactus/', views.contactus, name="contactus"),
-    path('address/', views.address, name= "address"),
 ]
--- firstprojectt/views.py
+++ firstprojectt/views.py
@@ -25,6 +25,41 @@ def task(request):
     }
     return render(request, "task.html", context)
 
+def del_task(request, task_id):
+    task_obj = Task.objects.get(id = task_id)
+    task_obj.delete()
+    messages.success(request, "Task deleted successfully from the list")
+    return redirect("task")
+
+def edit_task(request, task_id):
+    task_obj = Task.objects.get(id = task_id)
+    if request.method == "POST":
+        form_data = TaskForm(request.POST or None, instance=task_obj)
+        if form_data.is_valid():
+            form_data.save()
+            messages.success(request, "Task updated successfully to the list")
+            return redirect("task")
+        
+
+    context = {
+        'task_obj' : task_obj
+    }
+    return render(request, "edit.html", context)
+
+def comp_task(request, task_id):
+    task_obj = Task.objects.get(id = task_id)
+    task_obj.is_completed = True
+    task_obj.save()
+    messages.success(request, "Complete Status Updated")
+    return redirect("task")
+
+def pend_task(request, task_id):
+    task_obj = Task.objects.get(id = task_id)
+    task_obj.is_completed = False
+    task_obj.save()
+    messages.success(request, "Complete Status Updated")
+    return redirect("task")
+
 def aboutus(request):
     context = {
         'page' : "About"
@@ -36,8 +71,3 @@ def contactus(request):
         'page' : "Contact"
     }
     return render(request, "contact.html", context)
-def address(request):
-    context = {
-        'page' : "Address"
-    }
-    return render(request, "address.html", context)
